6b92469cae8590fe8a9a8181475165eb0b8f2fe0
Liam Fries <lfries@olin.edu>
2026-02-21 22:49:52 -0500

    servo stuff works??

--- Punishment/Punishment.ino
+++ Punishment/Punishment.ino
@@ -1,15 +1,17 @@
 #include <LiquidCrystal.h>
+#include <Servo.h>
 
 // RS, E, D4, D5, D6, D7
-LiquidCrystal lcd(7, 6, 5, 4, 3, 2);
+LiquidCrystal lcd(7, 8, 5, 4, 3, 2);
 
-int PRESS_PIN = A1;
+Servo myServo;
+
 int COM_PIN = A2;
-int pressure;
+bool lights = false;
 
-#define RED 9
-#define GREEN 10
-#define BLUE 11
+int RED = 11;
+int GREEN = 10;
+int BLUE = 9;
 
 int speed = 1; // lower = faster, higher = slower (ms per step)
 
@@ -27,19 +29,24 @@ void setup() {
   lcd.createChar(0, boom);
   lcd.begin(16, 2); // 16 columns, 2 rows
   lcd.print("Hello Brandon!");
-  pinMode(PRESS_PIN, INPUT);
   pinMode(COM_PIN, INPUT);
   pinMode(RED, OUTPUT);
   pinMode(GREEN, OUTPUT);
   pinMode(BLUE, OUTPUT);
+  myServo.attach(6);
 }
 
 void punish() {
   unsigned long currentMillis = millis();
-
+  // rotate to 90°
+  myServo.write(0);    // rotate to 0°
+  delay(1000);
+  myServo.write(90);   // rotate to 90°
+  delay(1000);
+  myServo.write(180);  // rotate to 180°
+  delay(1000);
   // ---- Non-blocking LCD update ----
-  if (currentMillis - lastLCDUpdate >= lcdInterval) {
-    lastLCDUpdate = currentMillis;
+  if (currentMillis - lastLCDUpdate >= 0.5*lcdInterval) {
     lcd.clear();
     lcd.write("YOU GOT SLAPPED");
     lcd.setCursor(6, 1);
@@ -48,25 +55,38 @@ void punish() {
     lcd.write(byte(0));
     // delay(200); --> we want the rest of the program to continue, but this
     // should wait 200 ms before updating
+  } else if (currentMillis - lastLCDUpdate >= lcdInterval) {
+    lastLCDUpdate = currentMillis;
+    lcd.clear();
+    lcd.write("BOOOM");
+    lcd.setCursor(6, 1);
+    lcd.write(byte(0));
+    lcd.write(byte(0));
+    lcd.write(byte(0));
   }
-  int zeroedCount = i % (255 * 3);
-  int loop = zeroedCount / 255;
-  int loopValue = zeroedCount % 255;
-
-  analogWrite(RED, (loop == 0) * (loopValue) + (loop == 1) * (255) +
-                       (loop == 2) * (255 - loopValue));
-  analogWrite(GREEN, (loop == 0) * (255 - loopValue) +
-                         (loop == 1) * (loopValue) + (loop == 2) * (255));
-  analogWrite(BLUE, (loop == 0) * (255) + (loop == 1) * (255 - loopValue) +
-                        (loop == 2) * (loopValue));
-
-  i++;
-  delay(speed);
+  lights = true;
+  Serial.println(lights);
 }
 
 void loop() {
-
+  punish();
   if (analogRead(COM_PIN) > 512) {
     punish();
+  }
+  if (lights == true) {
+    int zeroedCount = i % (255 * 3);
+    int loop = zeroedCount / 255;
+    int loopValue = zeroedCount % 255;
+
+    analogWrite(RED, (loop == 0) * (loopValue) + (loop == 1) * (255) +
+                        (loop == 2) * (255 - loopValue));
+    analogWrite(GREEN, (loop == 0) * (255 - loopValue) +
+                          (loop == 1) * (loopValue) + (loop == 2) * (255));
+    analogWrite(BLUE, (loop == 0) * (255) + (loop == 1) * (255 - loopValue) +
+                          (loop == 2) * (loopValue));
+    Serial.println(loopValue);
+
+    i++;
+    delay(speed);
   }
 }
